f5ec5dab524c82214b273721d3df6e64e4e4204b
Mike Blackstock <mike (at) omet.ca>
2020-03-30 09:04:03 -0400

    initial commit

---
+++ locales.js
@@ -0,0 +1,83 @@
+export const en_EN = {
+  LBL_SHOW_HIDDEN_FILES: 'Show hidden files',
+  LBL_MINIMALISTIC: 'Minimalistic',
+  LBL_OPEN_WITH: 'Open with...',
+  LBL_SHOW_DATE: 'Show date column',
+  LBL_STATUS: '{0} directories, {1} files, {2} bytes total',
+  LBL_DATE: 'Date', // FIXME: Move to client
+  LBL_LOADING: 'Loading {0}',
+  DIALOG_MKDIR_MESSAGE: 'Create new directory',
+  DIALOG_MKDIR_PLACEHOLDER: 'New directory',
+  DIALOG_RENAME_MESSAGE: 'Rename {0} ?',
+  DIALOG_DELETE_MESSAGE: 'Delete {0} ?',
+  MSG_ERROR: 'An error occurred',
+  MSG_UPLOAD_ERROR: 'Failed to upload file(s)',
+  MSG_READDIR_ERROR: 'An error occurred while reading directory: {0}',
+  MSG_PASTE_ERROR: 'Failed to paste file(s)',
+  MSG_MKDIR_ERROR: 'Failed to create directory',
+  MSG_RENAME_ERROR: 'Failed to rename',
+  MSG_DELETE_ERROR: 'Failed to delete'
+};
+
+export const nb_NO = {
+  LBL_SHOW_HIDDEN_FILES: 'Vis skjulte filer',
+  LBL_MINIMALISTIC: 'Minimalistisk',
+  LBL_OPEN_WITH: 'Åpne med...',
+  LBL_SHOW_DATE: 'Vis dato kolonne',
+  LBL_STATUS: '{0} mapper, {1} filer, {2} bytes totalt',
+  LBL_DATE: 'Dato',
+  LBL_LOADING: 'Laster {0}',
+  DIALOG_MKDIR_MESSAGE: 'Lag ny mappe',
+  DIALOG_MKDIR_PLACEHOLDER: 'Ny mappe',
+  DIALOG_RENAME_MESSAGE: 'Omdøpe {0} ?',
+  DIALOG_DELETE_MESSAGE: 'Slette {0} ?',
+  MSG_ERROR: 'En feil oppstod',
+  MSG_UPLOAD_ERROR: 'Feil under opplasting av fil(er)',
+  MSG_READDIR_ERROR: 'En feil oppstod under lesing av mappe: {0}',
+  MSG_PASTE_ERROR: 'Feil oppstod under liming av fil(er)',
+  MSG_MKDIR_ERROR: 'Feil oppstod under opprettelse av mappe',
+  MSG_RENAME_ERROR: 'Feil oppstod under omdøping',
+  MSG_DELETE_ERROR: 'Feil oppstod under sletting'
+};
+
+export const vi_VN = {
+  LBL_SHOW_HIDDEN_FILES: 'Hiển thị tập tin ẩn',
+  LBL_MINIMALISTIC: 'Tối giản',
+  LBL_OPEN_WITH: 'Mở bằng...',
+  LBL_SHOW_DATE: 'Hiện cột thời gian',
+  LBL_STATUS: '{0} thư mục, {1} tập tin, tổng dung lượng {2} bytes',
+  LBL_DATE: 'Thời gian',
+  LBL_LOADING: 'Đang tải {0}',
+  DIALOG_MKDIR_MESSAGE: 'Tạo thư mục mới',
+  DIALOG_MKDIR_PLACEHOLDER: 'Thư mục mới',
+  DIALOG_RENAME_MESSAGE: 'Đổi tên {0} ?',
+  DIALOG_DELETE_MESSAGE: 'Xoá {0} ?',
+  MSG_ERROR: 'Đã xảy ra lỗi',
+  MSG_UPLOAD_ERROR: 'Không thể tải lên tập tin',
+  MSG_READDIR_ERROR: 'Đã xảy ra lỗi trong khi đọc thư mục: {0}',
+  MSG_PASTE_ERROR: 'Không thể dán tập tin',
+  MSG_MKDIR_ERROR: 'Không thể tạo thư mục',
+  MSG_RENAME_ERROR: 'Không thể đổi tên',
+  MSG_DELETE_ERROR: 'Không thể xóa'
+};
+
+export const pt_BR = {
+  LBL_SHOW_HIDDEN_FILES: 'Mostrar arquivos ocultos',
+  LBL_MINIMALISTIC: 'Minimalista',
+  LBL_OPEN_WITH: 'Abrir com...',
+  LBL_SHOW_DATE: 'Mostrar coluna Data',
+  LBL_STATUS: '{0} diretórios, {1} arquivos, {2} bytes no total',
+  LBL_DATE: 'Data', // FIXME: Move to client
+  LBL_LOADING: 'Carregando {0}',
+  DIALOG_MKDIR_MESSAGE: 'Criar novo diretório',
+  DIALOG_MKDIR_PLACEHOLDER: 'Novo diretório',
+  DIALOG_RENAME_MESSAGE: 'Renomear {0} ?',
+  DIALOG_DELETE_MESSAGE: 'Deletar {0} ?',
+  MSG_ERROR: 'Um erro ocorreu',
+  MSG_UPLOAD_ERROR: 'Falha ao fazer upload do(s) arquivo(s)',
+  MSG_READDIR_ERROR: 'Um erro ocorreu ao ler o diretório: {0}',
+  MSG_PASTE_ERROR: 'Falha ao colar arquivo(s)',
+  MSG_MKDIR_ERROR: 'Falha ao criar diretório',
+  MSG_RENAME_ERROR: 'Falha ao renomear',
+  MSG_DELETE_ERROR: 'Falha ao deletar'
+};
